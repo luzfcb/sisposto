c4ef7cb3b2063e28d35bf0b7689447f9c0dc43cf
Fábio C. Barrioneuvo da Luz <bnafta@gmail.com>
2014-09-16 09:38:22 -0300

    limpeza de arquivos antigos

--- sisposto/static/js/setlang.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".btn-setlang").click(function(e) {
-        var form = $(".set_language");
-        form.find("input[name='language']").val(
-            $(this).attr("data-language")
-        );
-        form.submit();
-    });
-});
