c905091c1042d7490cb1a9bfd93b50549f64d329
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2014-09-19 12:50:02 -0300

    adicionado views para testes e template teste

--- sisposto/core/views.py
+++ sisposto/core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from braces.views import AjaxResponseMixin
 
 # Create your views here.
+from django.views import generic
+
+
+class Home(generic.TemplateView):
+    template_name = 'account/settings.html'
+
+
+class NotificationsView(AjaxResponseMixin, generic.TemplateView):
+    template_name = 'smartadmin/ajax/notify/notifications.html'
+
+
+class TasksNotifyView(AjaxResponseMixin, generic.TemplateView):
+    template_name = 'smartadmin/ajax/notify/tasks.html'
+
+
+class MailNotifyView(AjaxResponseMixin, generic.TemplateView):
+    template_name = 'smartadmin/ajax/notify/mail.html'
+
+
+class ModalVoicecommandView(AjaxResponseMixin, generic.TemplateView):
+    template_name = 'smartadmin/ajax/modal-content/modal-voicecommand.html'
--- sisposto/urls.py
+++ sisposto/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from core.views import Home
+from core.views import Home, MailNotifyView, TasksNotifyView, NotificationsView, ModalVoicecommandView
 
 
 urlpatterns = patterns('',
@@ -16,10 +16,24 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^/$',
-        Home.as_view()
-        ),
     url(r'^account/', include('account.urls')),
+
+    url(r'^ajax/notify/mail/',
+        MailNotifyView.as_view(),
+        ),
+    url(r'^ajax/notify/notifications/',
+        NotificationsView.as_view(),
+        ),
+    url(r'^ajax/notify/tasks/',
+        TasksNotifyView.as_view(),
+        ),
+    url(r'^ajax/modal-content/modal-voicecommand/',
+        ModalVoicecommandView.as_view(),
+        ),
+    url(r'^$',
+        Home.as_view(),
+        name='home_view'
+        ),
 )
 
 
